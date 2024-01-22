5962aa8b1b5d38ae5b96d57e328a0853053087f5
Aritra45 <144058048+Aritra45@users.noreply.github.com>
2024-01-22 16:02:16 +0530

    File Uploaded

---
+++ routes/userRoutes.js
@@ -0,0 +1,15 @@
+// userRoute.js
+const express = require("express");
+const {
+  loginController,
+  registerController,
+} = require("./../controllers/userController");
+
+const router = express.Router();
+
+// Existing routes
+router.post("/login", loginController);
+router.post("/register", registerController);
+
+
+module.exports = router;
